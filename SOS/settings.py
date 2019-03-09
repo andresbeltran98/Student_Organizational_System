55646dcd634283ea6fa57017992af87ba54ebe56
Andres Beltran <andresbeltran19@hotmail.com>
2019-03-09 17:46:07 -0500

    home.html

--- SOS/settings.py
+++ SOS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'USERS.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
