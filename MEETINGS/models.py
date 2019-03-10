ad0588a11fcb2a3ae640e88abc7cf54c5d595844
Andres Beltran <andresbeltran19@hotmail.com>
2019-03-10 00:50:28 -0500

    Meetings, membership databases created

--- MEETINGS/models.py
+++ MEETINGS/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Meeting(models.Model):
+
+    title = models.CharField(max_length=100)
+    university = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    members = models.ManyToManyField(User, through='Membership')
+
+    def __str__(self):
+        return self.title
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Meeting, on_delete=models.CASCADE)
+    date_joined = models.DateField()
