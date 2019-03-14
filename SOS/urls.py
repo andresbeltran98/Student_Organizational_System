2ff05499d655696c78d7a518ceb3cc8882db3b12
Andres Beltran <andresbeltran19@hotmail.com>
2019-03-14 14:23:38 -0400

    bootstrap added

--- SOS/urls.py
+++ SOS/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('USERS.urls')),
-    path('mf/', include('MEETINGS.urls'))
+    #path('', include('USERS.urls')),
+    path('', include('MEETINGS.urls'))
 ]
