c3399f9363c656e704ad8b3d6a498c4f28125a6f
Andres Beltran <andresbeltran19@hotmail.com>
2019-03-19 01:53:13 -0400

    Merge before demo1

--- MEETINGS/tests/test_views.py
+++ MEETINGS/tests/test_views.py
@@ -7,8 +7,32 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
         self.create_url = reverse('meeting-create')
+        self.list_url = reverse('meetings-list')
+        self.search_url = reverse('meeting-search')
+        self.detail_url = reverse('meeting-detail', args=['1'])
+        self.update_url = reverse('meeting-update', args=['1'])
+        self.delete_url = reverse('meeting-delete', args=['1'])
 
-    def test_meeting_list_GET(self):
+    def test_meeting_create_GET(self):
         response = self.client.get(self.create_url)
         self.assertEquals(response.status_code, 302)
-        self.assertTemplateUsed(response, 'meeting_form.html')
+
+    def test_meeting_list_GET(self):
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_meeting_search_GET(self):
+        response = self.client.get(self.search_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_meeting_detail_GET(self):
+        response = self.client.get(self.detail_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_meeting_update_GET(self):
+        response = self.client.get(self.update_url)
+        self.assertEquals(response.status_code, 302)
+
+    def test_meeting_delete_GET(self):
+        response = self.client.get(self.delete_url)
+        self.assertEquals(response.status_code, 302)
