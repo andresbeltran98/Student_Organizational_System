30c262621296c8c9cf021a3e0dd4a92f4e5a72e3
Thomas Patton <tjpatton1@gmail.com>
2019-03-26 21:31:50 -0400

    Profile Changes, media folder added

--- SOS/urls.py
+++ SOS/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from USERS import views as user_views
 from django.contrib.auth import views as auth_views
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('', include('MEETINGS.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- USERS/apps.py
+++ USERS/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'USERS'
+
+    def ready(self):
+        import USERS.signals
